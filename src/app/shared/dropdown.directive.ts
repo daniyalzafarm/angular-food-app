0438e877c1d4b3eafec45afb4dd0c5ba8d010861
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-01-10 15:24:27 +0500

    Link Bootstrap JS file

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,19 @@
+// This Directive is not Usable as we add BootstrapJS link to make dropdown workable
+
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+  constructor(private elementRef: ElementRef) {}
+}
